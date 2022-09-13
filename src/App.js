6322afc29e041653096e9b27bc9db0295594c8d7
Ronaldo Reis <ronaldocreis7@gmail.com>
2022-09-13 19:45:33 -0300

    hook botao

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import React from "react";
+
 const App = () => {
+  const [ativo, setAtivo] = React.useState(true);
+
+  function handleClick() {
+    setAtivo(!ativo);
+  }
+
   return (
-    <>
-      <p>Teste</p>
-    </>
+    <button onClick={handleClick} disabled={!ativo}>
+      {ativo ? "Ativo" : "Inativo"}
+    </button>
+    // <></>
   );
 };
 export default App;
