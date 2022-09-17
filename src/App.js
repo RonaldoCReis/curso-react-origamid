3224026deb27ba47879c47306e03e0a72f983fc9
Ronaldo Reis <ronaldocreis7@gmail.com>
2022-09-16 23:04:01 -0300

    useCallback

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React from "react";
 
 const App = () => {
-  const [carrinho, setCarrinho] = React.useState(0);
-  const [notificacao, setNotificacao] = React.useState(null);
-  const timeoutRef = React.useRef();
-  function handleClick() {
-    setCarrinho(carrinho + 1);
-    setNotificacao("Item adicionado ao carrinho");
-    clearTimeout(timeoutRef.current);
-    timeoutRef.current = setTimeout(() => {
-      setNotificacao(null);
-    }, 2000);
-  }
+  const [contar, setContar] = React.useState(0);
+
+  const handleClick = React.useCallback(() => {
+    setContar((contar) => contar + 1);
+  }, []);
+
   return (
     <div>
-      <p>{notificacao}</p>
-      <button onClick={handleClick}>Adicionar Carrinho ({carrinho})</button>
+      <button onClick={handleClick}>{contar}</button>
     </div>
   );
 };
